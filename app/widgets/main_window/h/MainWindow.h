dcdd4605f6a82fc3d9c01cd144ef8c349b6e8cec
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-08-09 06:08:31 +0200

    Add Yearly Dashboard widget, menu, and  actions to MainWindow

--- app/widgets/main_window/h/MainWindow.h
+++ app/widgets/main_window/h/MainWindow.h
@@ -13,6 +13,8 @@
 #include "AddGroceryBillWidget.h"
 #include "SearchGroceryBillWidget.h"
 
+#include "YearlyOverviewDashboardWidget.h"
+
 class MainWindow: public QMainWindow
 {
     Q_OBJECT
@@ -26,31 +28,54 @@ private slots:
     void searchGroceryBill();
     void addGroceryStore();
     void searchGroceryStore();
+    void addBillItem();
+    void searchBillItem();
     void addItemCategory();
     void searchItemCategory();
+    void viewDashboardYearlyOverview();
+    void viewDashboardMonthlyOverview();
 
 
 private:
     // Menus:
     QMenu* m_add_menu;
     QMenu* m_search_menu;
+    QMenu* m_dashboard_menu;
+    QMenu* m_reports_menu;
 
     // Actions:
     QAction* m_add_grocery_bill;
     QAction* m_search_grocery_bill;
     QAction* m_add_grocery_store;
     QAction* m_search_grocery_store;
+    QAction* m_add_bill_item;
+    QAction* m_search_bill_item;
     QAction* m_add_item_category;
     QAction* m_search_item_category;
+    QAction* m_view_dashboard_yearly_overview;
+    QAction* m_view_dashboard_monthly_overview;
 
     // Widgets:
     QStackedWidget* m_central_widget;
     AddGroceryBillWidget* m_add_grocery_bill_widget;
-    SearchGroceryBillWidget* m_search_grocery_bill_widget; 
+    SearchGroceryBillWidget* m_search_grocery_bill_widget;
+    YearlyOverviewDashboardWidget* m_yearly_overview_dashbaord_widget;
 
     // Private methods:
     void createUi();
     void createActions();
     void createMenus();
+
+    // TODO: Rename 'void clearFields()' to 'void resetFields()'
+    
+    // TODO: Should I remove BillItem from Add & Search Menus? 
+    //       Should I keep a database of all items purchased? 
+    //       How will I handle discounts? 
+    //       What abour price hikes after inflation??
+    //       Think about it carefully!
+    // Answer: Product table for standard product, 
+    //         Bill Item table for instance of product(it covers discounts, price hikes, etc.)
+    //         Look at ChatGPT answer for more details
+    // TODO: Thus, replace BillItem in Add & Search Menu with Product entity 
 
 };
--- app/widgets/main_window/src/MainWindow.cpp
+++ app/widgets/main_window/src/MainWindow.cpp
@@ -19,10 +19,12 @@ void MainWindow::createUi() {
     
     m_add_grocery_bill_widget = new AddGroceryBillWidget(this);
     m_search_grocery_bill_widget = new SearchGroceryBillWidget(this);
+    m_yearly_overview_dashbaord_widget = new YearlyOverviewDashboardWidget(this);
     
     m_central_widget = new QStackedWidget(this);
     m_central_widget->addWidget(m_add_grocery_bill_widget);
     m_central_widget->addWidget(m_search_grocery_bill_widget);
+    m_central_widget->addWidget(m_yearly_overview_dashbaord_widget);
 
     m_central_widget->setCurrentWidget(m_search_grocery_bill_widget);
     this->setCentralWidget(m_central_widget);
@@ -46,6 +48,11 @@ void MainWindow::createActions() {
     m_add_grocery_store->setToolTip(tr(ADD_GROCERY_STORE_TEXT));
     connect(m_add_grocery_store, &QAction::triggered, this, &MainWindow::addGroceryStore);
 
+    m_add_bill_item = new QAction(tr("&Bill Item"), this);
+    m_add_bill_item->setStatusTip(tr(ADD_BILL_ITEM_TEXT));
+    m_add_bill_item->setToolTip(tr(ADD_BILL_ITEM_TEXT));
+    connect(m_add_bill_item, &QAction::triggered, this, &MainWindow::addBillItem);
+
     m_add_item_category = new QAction(tr("&Item Category"), this);
     m_add_item_category->setStatusTip(tr(ADD_ITEM_CATEGORY_TEXT));
     m_add_item_category->setToolTip(tr(ADD_ITEM_CATEGORY_TEXT));
@@ -63,11 +70,26 @@ void MainWindow::createActions() {
     m_search_grocery_store->setToolTip(tr(SEARCH_GROCERY_STORE_TEXT));
     connect(m_search_grocery_store, &QAction::triggered, this, &MainWindow::searchGroceryStore);
 
+    m_search_bill_item = new QAction(tr("&Bill Item"), this);
+    m_search_bill_item->setStatusTip(tr(SEARCH_BILL_ITEM_TEXT));
+    m_search_bill_item->setToolTip(tr(SEARCH_BILL_ITEM_TEXT));
+    connect(m_search_bill_item, &QAction::triggered, this, &MainWindow::searchBillItem);
+
     m_search_item_category = new QAction(tr("&Item Category"), this);
     m_search_item_category->setStatusTip(tr(SEARCH_ITEM_CATEGORY_TEXT));
     m_search_item_category->setToolTip(tr(SEARCH_ITEM_CATEGORY_TEXT));
     connect(m_search_item_category, &QAction::triggered, this, &MainWindow::searchItemCategory);
 
+    // Dashboard Menu Actions:
+    m_view_dashboard_yearly_overview = new QAction(QIcon::fromTheme("view-dashboard"), tr("&Yearly Overview"), this);
+    m_view_dashboard_yearly_overview->setStatusTip(tr(VIEW_DASHBOARD_YEARLY_OVERVIEW_TEXT));
+    m_view_dashboard_yearly_overview->setToolTip(tr(VIEW_DASHBOARD_YEARLY_OVERVIEW_TEXT));
+    connect(m_view_dashboard_yearly_overview, &QAction::triggered, this, &MainWindow::viewDashboardYearlyOverview);
+    
+    m_view_dashboard_monthly_overview = new QAction(QIcon::fromTheme("x-office-document"), tr("&Monthly Overview"), this);
+    m_view_dashboard_monthly_overview->setStatusTip(tr(VIEW_DASHBOARD_MONTHLY_OVERVIEW_TEXT));
+    m_view_dashboard_monthly_overview->setToolTip(tr(VIEW_DASHBOARD_MONTHLY_OVERVIEW_TEXT));
+    connect(m_view_dashboard_monthly_overview, &QAction::triggered, this, &MainWindow::viewDashboardMonthlyOverview);
 }
 
 
@@ -76,12 +98,20 @@ void MainWindow::createMenus() {
     m_add_menu = menuBar()->addMenu(tr("&Add"));
     m_add_menu->addAction(m_add_grocery_bill);
     m_add_menu->addAction(m_add_grocery_store);
+    m_add_menu->addAction(m_add_bill_item);
     m_add_menu->addAction(m_add_item_category);
 
     m_search_menu = menuBar()->addMenu(tr("&Search"));
     m_search_menu->addAction(m_search_grocery_bill);
     m_search_menu->addAction(m_search_grocery_store);
+    m_search_menu->addAction(m_search_bill_item);
     m_search_menu->addAction(m_search_item_category);
+
+    m_dashboard_menu = menuBar()->addMenu(tr("&Dashboard"));
+    m_dashboard_menu->addAction(m_view_dashboard_yearly_overview);
+    m_dashboard_menu->addAction(m_view_dashboard_monthly_overview);
+
+    m_reports_menu = menuBar()->addMenu(tr("&Reports"));
 }
 
 
@@ -112,6 +142,16 @@ void MainWindow::searchGroceryStore() {
 
 }
 
+void MainWindow::addBillItem() {
+    qDebug() << "Add Bill Item clicked!\n";
+
+}
+
+void MainWindow::searchBillItem() {
+    qDebug() << "Search Bill Item clicked!\n";
+
+}
+
 void MainWindow::addItemCategory() {
     qDebug() << "Search Grocery Store clicked!\n";
 
@@ -122,3 +162,13 @@ void MainWindow::searchItemCategory() {
 
 }
 
+void MainWindow::viewDashboardYearlyOverview() {
+    qDebug() << "View Dashboard (Yearly Overview) clicked!\n";
+
+    m_central_widget->setCurrentWidget(m_yearly_overview_dashbaord_widget);
+}
+
+void MainWindow::viewDashboardMonthlyOverview() {
+    qDebug() << "View Dashboard (Monthly Overview) clicked!\n";
+    
+}
