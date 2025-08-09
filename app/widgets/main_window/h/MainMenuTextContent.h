fb4a262aaee7f790066df98d37e0381b7a8fb4f9
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-08-09 06:09:18 +0200

    Add Dashboard text content; Update other text content

--- app/widgets/main_window/h/MainMenuTextContent.h
+++ app/widgets/main_window/h/MainMenuTextContent.h
@@ -3,8 +3,12 @@
 #include <QString>
 
 const auto ADD_GROCERY_BILL_TEXT = "Add a new Grocery Bill";
-const auto SEARCH_GROCERY_BILL_TEXT = "Search a Grocery Bill";
+const auto SEARCH_GROCERY_BILL_TEXT = "Search Grocery Bills";
 const auto ADD_GROCERY_STORE_TEXT = "Add a new Grocery Store";
-const auto SEARCH_GROCERY_STORE_TEXT = "Search a Grocery Store";
+const auto SEARCH_GROCERY_STORE_TEXT = "Search Grocery Stores";
+const auto ADD_BILL_ITEM_TEXT = "Add a new Bill Item";
+const auto SEARCH_BILL_ITEM_TEXT = "Search Bill Items";
 const auto ADD_ITEM_CATEGORY_TEXT = "Add a new Item Category";
-const auto SEARCH_ITEM_CATEGORY_TEXT = "Search an Item Category";
+const auto SEARCH_ITEM_CATEGORY_TEXT = "Search Item Categories";
+const auto VIEW_DASHBOARD_YEARLY_OVERVIEW_TEXT = "View spending details for the past year";
+const auto VIEW_DASHBOARD_MONTHLY_OVERVIEW_TEXT = "View detailed spending details for a selected month";
