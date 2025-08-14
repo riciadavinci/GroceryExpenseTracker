676afddf6345573c3c046ee63cefebfe493d1f16
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-08-14 19:11:19 +0200

    Add seeds for: Units, Categories, SubCategories

---
+++ app/resources/sql/seeds/02_categories.sql
@@ -0,0 +1,13 @@
+INSERT OR IGNORE INTO categories (name) VALUES 
+    ('Meat & Poultry'),
+    ('Dairy'),
+    ('Grains'),
+    ('Premade, Instant, & Frozen'),
+    ('Snacks & Junk'),
+    ('Produce'),
+    ('Drinks'),
+    ('Bakery & Baked Goods'),
+    ('Condiments & Sauces'),
+    ('Herbs & Spices'),
+    ('Oils & Fats'),
+    ('Miscellaneous');
