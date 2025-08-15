baad6dfdda4508a7802099bff132e812248d3cc4
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-08-15 21:29:44 +0200

    Seed table: products

---
+++ app/resources/sql/seeds/04_products.sql
@@ -0,0 +1,15 @@
+INSERT OR IGNORE INTO products (id, category_id, sub_category_id, unit_id, product_name, variant, brand_name, nutri_score) VALUES 
+    ('PR-c34fec19-1068-4d16-9689-90b890fd4b77', 3, 12, 1, 'Parboiled Rice', NULL, 'BON-RI', 'B'),
+    ('PR-49c495af-d01c-412c-bfc4-817a922b3a69', 3, 12, 1, 'Parboiled Rice', NULL, 'Bonrisi', 'B'),
+    ('PR-19edfe42-6a20-4da0-a16a-73fdd8031511', 3, 12, 1, 'Basmati Rice', NULL, 'BON-RI', 'B'),
+    ('PR-eedca02e-3057-4289-ac31-48f2f20958e7', 3, 15, 2, 'Spaghetti', NULL, 'Cucina Nobile', 'A'),
+    ('PR-936bb1ea-c182-4c74-940f-abb8514dc419', 3, 15, 2, 'Penne', NULL, 'Cucina Nobile', 'A'),
+    ('PR-82991a9f-404c-4f92-a0c5-68935886fd85', 3, 15, 2, 'Hamburger Brötchen', NULL, 'Goldhähren', 'C'),
+    ('PR-1b517c12-7da2-4ef8-acf1-28ba915df288', 3, 15, 2, 'Protein Brot', NULL, 'Goldhähren', 'A');
+    -- ('PR-', 3, 12, 1, '', NULL, NULL),
+    -- ('PR-', 1, 1, 1, '', NULL, NULL),
+    -- ('PR-', 1, 1, 1, '', NULL, NULL),
+    -- ('PR-', 1, 1, 1, '', NULL, NULL),
+    -- ('PR-', 1, 1, 1, '', NULL, NULL),
+    -- ('PR-', 1, 1, 1, '', NULL, NULL),
+    -- ('PR-', 1, 1, 1, '', NULL, NULL);
