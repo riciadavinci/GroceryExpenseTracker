1343e247ae3566d816bcaf81aad2c019cdcb326d
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-08-15 21:30:44 +0200

    Seed tables: grocery_stores & store_types

---
+++ app/resources/sql/seeds/06_store_types.sql
@@ -0,0 +1,7 @@
+INSERT OR IGNORE INTO TABLE store_types (name) VALUES 
+    ('Supermarket'),
+    ('Convenience Store'),
+    ('Asian Supermarket'),
+    ('Indian Supermarket'),
+    ('Turkish Supermarket'),
+    ('Bakery');
---
+++ app/resources/sql/seeds/07_grocery_stores.sql
@@ -0,0 +1,7 @@
+INSERT OR IGNORE INTO grocery_stores (id, name, type_id) VALUES 
+    ('GS-ceb7e0e1-eda9-4037-8001-f6f4191cbf02', 'Aldi Süd', 1),
+    ('GS-a7b291ba-d0b4-4f02-b67f-cf26b6400e59', 'Netto', 1),
+    ('GS-ff4585be-d430-4428-bc8f-abf91f0c9587', 'REWE', 1),
+    ('GS-6c2bfe86-b4a8-4d09-b237-7504fe014f64', 'Kaufland', 1),
+    ('GS-d973ab96-c1fc-4d42-9426-01a0fda12023', 'Lidl', 1),
+    ('GS-060fb472-cc7c-45a2-bda5-e481e5df245e', 'Edeka', 1);
