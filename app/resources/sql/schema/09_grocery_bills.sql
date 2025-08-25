79e94d26dc6ae0d72b4a34b055c93e30fbd3f4e4
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-08-25 19:05:17 +0200

    Rename Query files

--- app/resources/sql/schema/09_grocery_bills.sql
+++ app/resources/sql/schema/09_grocery_bills.sql
@@ -1,11 +1,14 @@
 CREATE TABLE IF NOT EXISTS grocery_bills (
     id TEXT PRIMARY KEY,
-    bill_date DATETIME NOT NULL,
     total REAL NOT NULL,
+    currency_id INTEGER NOT NULL,
+    bill_date DATE NOT NULL,
+    bill_time TIME NOT NULL,
     store_id TEXT NOT NULL,
     address_id TEXT NOT NULL,
     created_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (currency_id) REFERENCES currencies(id),
     FOREIGN KEY (store_id) REFERENCES grocery_stores(id),
     FOREIGN KEY (address_id) REFERENCES addresses(id)
 );
