62581cc46d548fedda1c22db20a94bcd572b964e
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-08-25 19:05:59 +0200

    Add Initial Seed for Currency

---
+++ app/resources/sql/seeds/08_currencies.sql
@@ -0,0 +1,13 @@
+INSERT OR IGNORE INTO currencies (name, code, symbol) VALUES 
+    ('Euro', 'EUR', '€'),
+    ('US Dollar', 'USD', '$'),
+    ('Indian Rupee', 'INR', '₹'),
+    ('Pound Sterling', 'GBP', '£'),
+    ('Australian Dollar', 'AUD', '$');
+    -- TODO: Add more currencies
+    -- ('', '', ''), 
+    -- ('', '', ''),
+    -- ('', '', ''),
+    -- ('', '', ''),
+    -- ('', '', ''),
+    -- ('', '', '');
