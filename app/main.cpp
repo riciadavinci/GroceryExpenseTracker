06caafcec9c3f6d2e3b4a07ca44dce4a3d22443a
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-08-26 02:00:09 +0200

    Include SqlQueryLoader instance in app

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <QApplication>
 #include "MainWindow.h"
+#include "SqlQueryLoader.h"
 
 int main(int argc, char** argv) {
     auto app = QApplication{argc, argv};
     auto window = MainWindow{nullptr};
+    auto sql_query_loader = SqlQueryLoader("resources/sql");
+
     window.show();
     return app.exec();
 }
